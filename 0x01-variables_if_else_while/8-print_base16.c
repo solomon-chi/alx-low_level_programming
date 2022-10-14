78aff6141a70f0c758e6851fef0bef25640088a9
solomon-chi <odosolomonchi@gmail.com>
2022-10-14 12:56:17 -0700

    making extra corrections

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,22 +3,21 @@
 #include <time.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 
 int main(void)
 {
-		
-	int d;
-	char low;
+		int d;
+			char low;
 
-	for (d = '0'; d <= '9'; d++)
-		 putchar(d);
-	for (low = 'a'; low <= 'f'; low++)
-		 putchar(low);
-		 putchar('\n');
+				for (d = '0'; d <= '9'; d++)
+							putchar(d);
+					for (low = 'a'; low <= 'f'; low++)
+								putchar(low);
+						putchar('\n');
 
-	return (0);
+							return (0);
 }
