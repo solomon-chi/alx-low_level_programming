069166a8fc97a69bdeaf235499bc5f7458bc9746
solomon-chi <odosolomonchi@gmail.com>
2022-10-24 04:18:48 -0700

    making corrections on the header file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,11 +9,4 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
 
